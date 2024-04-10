018bac11a34452765b6eab1ead99f53c701dd2ef
bradene0 <bradene0@outlook.com>
2024-04-09 19:27:24 -0500

    Initial Commit

---
+++ src/main/java/com/eyeD/eyed/EyedApplication.java
@@ -0,0 +1,13 @@
+package com.eyeD.eyed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eyeD/eyed/EyedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyeD.eyed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
